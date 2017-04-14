d5d44ec05f4b0f5dbba5f0c692c4bd34f2a115e6
chinhset <chnh9x.set@gmail.com>
2017-04-14 10:23:48 +0700

    Molecular Communication: Text message

---
+++ mRx/mRx.ino
@@ -0,0 +1,70 @@
+
+int sume = 0, j = 0, x = 0, a1, a2, b[7];
+char c[256];
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(A0, INPUT);
+  pinMode(3, INPUT);
+  Serial.begin(9600);
+  Serial.println("Start!!!");
+}
+
+void loop() {
+  if(!digitalRead(3)){
+    Serial.println("Start decoder!!!\n");
+    delay(900);
+    Decoder();
+  }
+}
+
+void Decoder(){
+  while(1){
+    for(int i = 0; i < 7; i++){
+      a1 = analogRead(A0);
+      Serial.print(a1);
+      delay(500);
+      a2 = analogRead(A0);
+      Serial.print(a2);
+//      if(i > 0){
+        if(a2 > a1 + 10){
+          b[i] = 1;
+          Serial.print("    bit: ");
+          Serial.print(b[i]);
+          Serial.print(";    ");
+          sume += 1;
+        }
+        else{
+          b[i] = 0;
+          Serial.print("    bit: ");
+          Serial.print(b[i]);
+          Serial.print(";     ");
+        }
+//      }
+      delay(700);
+    }
+    if(sume){
+      Serial.print("\nsume: ");
+      Serial.print(sume);
+      Serial.print(";     ");
+      for(int i =0; i < 7; i++){
+        x += (b[i] << (6 - i));
+      }
+      c[j] = char(x);
+      Serial.print("\nchar: ");
+      Serial.println(c[j]);
+      x = 0;
+      sume = 0;
+      j++;
+    }
+    else{
+      Serial.print("\nEnd decoder!!!  ");
+      Serial.print("Received message: "); 
+      for(int i = 0; i < j; i++)
+        Serial.print(c[i]);
+      j = 0;
+      sume = 0;
+      break;
+    }
+  }
+}
+
---
+++ mTx/mTx.ino
@@ -0,0 +1,48 @@
+int b[7];
+char sume = 0, c[256];
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2, OUTPUT);
+  Serial.begin(9600);
+  Serial.print("Start!!!\n");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial.available()){
+    char data = Serial.read();
+    Serial.print(Serial.available());
+    if(data == '!'){
+      Encoder();
+      Serial.print("\nSended message: ");
+      for(int i = 0; i < sume; i++)
+        Serial.print(c[i]);
+//      Serial.print("\nsume = ");
+//      Serial.print(sume);
+      sume = 0;
+    }
+    else{
+      c[sume] = data;
+      sume++;
+    }
+  }
+}
+
+void Encoder(){
+  digitalWrite(2, 1);
+  delay(200);
+  digitalWrite(2, 0);
+  delay(1000);
+  for(int i = 0; i < sume; i++){
+    for(int j = 6; j >= 0; j--){
+      b[j] = (c[i] >> j) & 1;
+      digitalWrite(2, b[j]);
+      Serial.print(b[j]);
+      delay(200);
+      digitalWrite(2, 0);
+      delay(1000);
+    }
+    Serial.println(c[i]);
+  }
+  digitalWrite(2, 0);
+}
